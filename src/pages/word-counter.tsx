1864278cb9d557e05e7b68f5828d2356056e7e69
poldro <paolo.valmori@gmail.com>
2023-02-02 02:06:15 +0100

    minor changes - overflow - h-textarea

--- src/pages/word-counter.tsx
+++ src/pages/word-counter.tsx
@@ -8,6 +8,7 @@ const CountWords: NextPage = () => {
 
     const [selectedWordCount, setSelectedWordCount] = useState(0);
     const [selectedCharacterCount, setSelectedCharacterCount] = useState(0);
+    const [selectedSentenceCount, setSelectedSentenceCount] = useState(0);
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setWordCount(e.target.value.split(' ').filter(word => word.length > 0).length);
@@ -19,15 +20,16 @@ const CountWords: NextPage = () => {
         const selection = window.getSelection() || '';
         setSelectedWordCount(selection.toString().split(/\s+/g).filter(Boolean).length);
         setSelectedCharacterCount(selection.toString().length);
+        setSelectedSentenceCount(selection.toString().split(/[.!?]+/g).length - 1);
     };
 
     return (
         <div className="flex flex-col justify-center items-center w-full h-screen">
-            <div className="w-full h-full max-w-3xl my-5 lg:my-8 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+            <div className="w-full h-full max-w-3xl my-5 lg:my-8 divide-y divide-gray-200 rounded-lg bg-white shadow">
                 <div className="px-4 py-5 sm:p-6">
                     <h1 className="text-center font-semibold text-3xl">Word Counter</h1>
                 </div>
-                <div className="px-4 py-4 sm:px-6 h-full">
+                <div className="px-4 py-4 sm:px-6 h-5/6">
                     <GrammarlyEditorPlugin clientId="client_2QNNYPesdJhwfCszntmPA6">
                         <textarea
                             onChange={handleChange}
@@ -39,8 +41,8 @@ const CountWords: NextPage = () => {
                     </GrammarlyEditorPlugin>
                 </div>
             </div>
-            <div>
-                <dl className="my-4 gap-4 grid grid-cols-3 w-full">
+            <div className="max-w-3xl w-full">
+                <dl className="my-4 gap-4 grid grid-cols-3">
 
                     <div className="flex flex-col justify-between rounded-lg bg-white px-4 py-5 shadow sm:p-6 text-center">
                         <dt className="text-sm font-medium text-gray-500">{selectedWordCount !== 0 && 'Selected'} Words</dt>
@@ -53,8 +55,8 @@ const CountWords: NextPage = () => {
                     </div>
 
                     <div className="flex flex-col justify-between overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6 text-center">
-                        <dt className="text-sm font-medium text-gray-500">Sentences</dt>
-                        <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{sentenceCount}</dd>
+                        <dt className="text-sm font-medium text-gray-500">{selectedSentenceCount !== 0 && 'Selected'} Sentences</dt>
+                        <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{selectedSentenceCount === 0 ? sentenceCount : selectedSentenceCount}</dd>
                     </div>
 
                 </dl>
