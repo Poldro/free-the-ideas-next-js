d5c414dd64dd33db4de215d445abbb1361ca9792
poldro <paolo.valmori@gmail.com>
2023-05-24 03:51:33 +0200

    fix GTM

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -6,13 +6,12 @@ const GoogleTagManager = () => {
     <>
       <Head>
         {/* The GTM script that goes into the head tag */}
-        <Script>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PQ7899Q');`}
-        </Script>
+})(window,document,'script','dataLayer','GTM-PQ7899Q');`}}
+        />
       </Head>
       <noscript>
         <iframe
