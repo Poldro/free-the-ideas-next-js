2931c5df1ea7b64c82da58a7b10b7e3191ef24de
poldro <paolo.valmori@gmail.com>
2023-05-24 04:14:50 +0200

    fix GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,31 +7,39 @@ import "react-toastify/dist/ReactToastify.css";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
-import GoogleTagManager from "../components/GoogleTagManager";
-
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <GoogleTagManager />
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={true}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-        toastStyle={{ backgroundColor: "#15162c" }}
-      />
-    </SessionProvider>
+    <>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PQ7899Q"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          toastStyle={{ backgroundColor: "#15162c" }}
+        />
+      </SessionProvider>
+    </>
   );
 };
 
